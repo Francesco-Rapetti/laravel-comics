c5cdb7d2db41f82ba251663c04e0eb737da2ccd5
Francesco-Rapetti <rapettif80@yahoo.it>
2024-01-24 00:42:55 +0100

    Implements footer

--- config/data.php
+++ config/data.php
@@ -179,5 +179,154 @@ return [
             'img' => 'images/buy-dc-power-visa.svg',
             'text' => 'DC POWER VISA'
         ],
+    ],
+
+    "footerLinks" => [
+        "link1" => [
+            "title" => "DC COMICS",
+            "links" => [
+                "link1" => [
+                    "name" => "Characters",
+                    "link" => "#"
+                ],
+                "link2" => [
+                    "name" => "Comics",
+                    "link" => "#"
+                ],
+                "link3" => [
+                    "name" => "Movies",
+                    "link" => "#"
+                ],
+                "link4" => [
+                    "name" => "TV",
+                    "link" => "#"
+                ],
+                "link5" => [
+                    "name" => "Games",
+                    "link" => "#"
+                ],
+                "link6" => [
+                    "name" => "Videos",
+                    "link" => "#"
+                ],
+                "link7" => [
+                    "name" => "News",
+                    "link" => "#"
+                ],
+            ]
+        ],
+
+        "link2" => [
+            "title" => "SHOP",
+            "links" => [
+                "link1" => [
+                    "name" => "Shop DC",
+                    "link" => "#"
+                ],
+                "link2" => [
+                    "name" => "Shop DC Collectibles",
+                    "link" => "#"
+                ]
+            ]
+        ],
+
+        "link3" => [
+            "title" => "DC",
+            "links" => [
+                "link1" => [
+                    "name" => "Terms Of Use",
+                    "link" => "#"
+                ],
+                "link2" => [
+                    "name" => "Privacy policy (New)",
+                    "link" => "#"
+                ],
+                "link3" => [
+                    "name" => "Ad Choices",
+                    "link" => "#"
+                ],
+                "link4" => [
+                    "name" => "Advertising",
+                    "link" => "#"
+                ],
+                "link5" => [
+                    "name" => "Jobs",
+                    "link" => "#"
+                ],
+                "link6" => [
+                    "name" => "Subscriptions",
+                    "link" => "#"
+                ],
+                "link7" => [
+                    "name" => "Talent Workshops",
+                    "link" => "#"
+                ],
+                "link8" => [
+                    "name" => "CPSC Certificates",
+                    "link" => "#"
+                ],
+                "link9" => [
+                    "name" => "Ratings",
+                    "link" => "#"
+                ],
+                "link10" => [
+                    "name" => "Shop Help",
+                    "link" => "#"
+                ],
+                "link11" => [
+                    "name" => "Contact Us",
+                    "link" => "#"
+                ]
+            ]
+        ],
+
+        "link4" => [
+            "title" => "SITES",
+            "links" => [
+                "link1" => [
+                    "name" => "DC",
+                    "link" => "#"
+                ],
+                "link2" => [
+                    "name" => "MAD Magazine",
+                    "link" => "#"
+                ],
+                "link3" => [
+                    "name" => "DC Kids",
+                    "link" => "#"
+                ],
+                "link4" => [
+                    "name" => "DC Universe",
+                    "link" => "#"
+                ],
+                "link5" => [
+                    "name" => "DC Power Visa",
+                    "link" => "#"
+                ]
+            ]
+        ]
+    ],
+
+    "socials" => [
+        "facebook" => [
+            "img" => "images/footer-facebook.png",
+            "link" => "#"
+        ],
+        "twitter" => [
+            "img" => "images/footer-twitter.png",
+            "link" => "#"
+        ],
+        "youtube" => [
+            "img" => "images/footer-youtube.png",
+            "link" => "#"
+        ],
+        "pinterest" => [
+            "img" => "images/footer-pinterest.png",
+            "link" => "#"
+        ],
+        "periscope" => [
+            "img" => "images/footer-periscope.png",
+            "link" => "#"
+        ]
     ]
 ];
